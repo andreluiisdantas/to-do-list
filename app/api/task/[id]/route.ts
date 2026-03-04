2164b324c959611b4e608e9d04c8f5dfabe2352c
andreluiisdantas <andreluissousaa10@gmail.com>
2026-03-04 14:45:10 -0300

    Created task routes

---
+++ app/api/task/[id]/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+import { listTaskController, updateTaskController, deleteTaskController } from "@/app/backend/controllers/task.controller";
+
+interface RouteParams{
+    params: {id: string};
+}
+
+// GET BY ID
+export async function GET(request: NextRequest, {params}: RouteParams) {
+    try{
+        const id = Number(params.id);
+        const task = await listTaskController(id);
+
+        if(!task) return NextResponse.json({error: "Tarefa não encontrada."}, {status: 404});
+        return NextResponse.json(task);
+    }catch(error){
+        return NextResponse.json({error: "Erro ao buscar usuário"}, {status: 500})
+    }
+}
+
+// PATCH
+export async function PATCH(request: NextRequest, { params }: RouteParams) {
+    try{
+        const id = Number(params.id);
+        const body = await request.json();
+        const updatedTask= await updateTaskController(id, body);
+
+        return NextResponse.json(updatedTask);
+    }catch (error){
+        return NextResponse.json({ error: "Erro ao atualizar"}, { status: 400});
+    }
+}
+
+// DELETE 
+export async function DELETE({ params }: RouteParams) {
+    try{
+        const id = Number(params.id);
+        await deleteTaskController(id);
+        return new NextResponse(null, {status: 204});
+    }catch (error){
+        return NextResponse.json({ error: "Erro ao deletar"}, { status: 500 });
+    }
+}
---
+++ app/api/task/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { listTasksController, createTaskController } from "@/app/backend/controllers/task.controller";
+
+// GET ALL
+export async function GET() {
+    try{
+        const tasks = await listTasksController();
+        return NextResponse.json(tasks)
+    }catch(error){
+        return NextResponse.json({error: "Erro ao encontrar a tarefa"}, {status: 500});
+    }
+}
+
+// POST
+export async function POST(request: NextRequest) {
+    try{
+        const body = await request.json();
+        const newTask = await createTaskController(body);
+        return NextResponse.json(newTask, {status: 201});
+    }catch(error){
+        return NextResponse.json({error: "Erro ao criar tarefa"}, {status: 400});
+    }
+}
